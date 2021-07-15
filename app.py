a6fbb46ba4708133e5d1a26099502b9331929a1e
Sarvar-Anvarov <53121543+Sarvar-Anvarov@users.noreply.github.com>
2021-07-15 13:57:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import faiss
 import time
 import altair as alt
@@ -8,7 +9,11 @@ import plotly.express as px
 from longtext import *
 from sentence_transformers import SentenceTransformer
 from scipy import stats
-import psutil, os
+import psutil, os, gc, json
+from warnings import filterwarnings
+from sklearn.metrics.pairwise import cosine_similarity
+
+filterwarnings('ignore')
 
 # Functions
 @st.cache(allow_output_mutation=True)
@@ -19,17 +24,30 @@ def search(query):
     index = faiss.IndexIDMap(faiss.IndexFlatIP(768))
     index.add_with_ids(encoded_data[test.index], np.array(range(0, len(test))))
     query_vector = model.encode([query])
-    top_k = index.search(query_vector, 20)
-    return [test.iloc[_id] for _id in top_k[1].tolist()[0]], top_k
+    top_k = index.search(query_vector, 30)
+    return [test.iloc[_id] for _id in top_k[1].tolist()[0]], top_k, query_vector
 
 def make_clickable(link, text):
     return f'<a target="_blank" href="{link}">{text}</a>'
 
+@st.cache(allow_output_mutation=True)
+def load_data(check):
+    if check:
+        counties = alt.topo_feature('https://raw.githubusercontent.com/Sarvar-Anvarov/streamlit-app/main/data/mo.json', 'collection')
+        source = 'https://raw.githubusercontent.com/Sarvar-Anvarov/streamlit-app/main/data/salary.csv'
+        data = pd.read_csv('data/data_all.csv')
+        map_data = pd.read_csv('data/map.csv')
+        full = pd.read_csv('data/full_all.csv')
+        encoded_data = pd.read_csv('data/full_all.csv').values.astype('float32', order = 'C')
+    
+    return counties, source, data, map_data, full, encoded_data
+
+
 # Data
-counties = alt.topo_feature('https://raw.githubusercontent.com/Sarvar-Anvarov/streamlit-app/main/data/mo.json', 'collection')
-source = 'https://raw.githubusercontent.com/Sarvar-Anvarov/streamlit-app/main/data/salary.csv'
-data = pd.read_csv('data/data.csv')
+counties, source, data, map_data, full, encoded_data = load_data(True)
 z = abs(stats.zscore(data['salary_from'],nan_policy = 'omit'))
+test = data['description']
+
 
 # Pages
 with st.sidebar:
@@ -38,7 +56,7 @@ with st.sidebar:
         st.write("")
     with col2:
         st.image("data/logo.png")
-    page = st.radio('Страница', ['Приветствие','Поиск вакансий','Интересная статистика'])
+    page = st.radio('Страница', ['Приветствие','Поиск вакансий','Интересная статистика','Карта'])
 
 
 # Page 1-Intro
@@ -46,40 +64,56 @@ if page=='Приветствие':
     st.markdown(dash, unsafe_allow_html = True)
     st.subheader('Приветствие')
     st.markdown(hello, unsafe_allow_html = True)
-    
 
 # Page 2
 if page=='Поиск вакансий':
     st.title('Поиск вакансий')
     st.subheader('Напиши про свои навыки, умения, увлечения и найди работу мечты!')
-
+    
+    
     # Sidebar and form
+    
+    exps=['Нет опыта','От 1 года до 3 лет','От 3 до 6 лет','Более 6 лет']
     with st.sidebar:
+        st.text('Фильтр недоступен ввиду малой базы \nданных')
         with st.form(key='form'):
             st.subheader('Фильтр параметров')
             city = st.multiselect('Город(а)', ['Москва', 'Санкт-Петербург'], default='Москва')
-            salary = st.slider('Диапазон зарплат, ₽', 0, 100000, (15000, 75000), step=5000)
-            exp = st.radio('Опыт работы', ['нет опыта','1-3 года','3-6 лет','более 6 лет'])
+            salary = st.slider('Диапазон зарплат, ₽', 0, 150000, (25000, 125000), step=5000)
+            exp = st.radio('Опыт работы', exps)
             submit_button = st.form_submit_button(label='Принять')
 
     # Loading model
     model = load_model('paraphrase-xlm-r-multilingual-v1')
-
-    # Preparing search function
-    encoded_data = pd.read_csv('data/full.csv').values.astype('float32', order = 'C')
-    test = data['description']
-    text = st.text_area('Только не скромничай!', value=example)
-    st.text('Топ вакансий по твоим навыкам')
     
-    results, top_k = search(str(text))
-    res = data[data['description'].isin(results)]
-
+    
+    # Preparing search function
+    text = st.text_area('Только не скромничай!', value=example)
+    st.text('Топ вакансий по твоим навыкам (модель в доработке, возможны некорректные вакансии)')
+    
+    results, top_k, vector = search(str(text))
+    res = data.loc[top_k[1][0]]
+    res['similarity'] = [cosine_similarity(vector, [full.loc[top_k[1][0]].loc[i]])[0][0] for i in full.loc[top_k[1][0]].index]
+    
+    res['similarity'] = (res['similarity']*100).round(1)
     res['job'] = res.apply(lambda x: make_clickable(x['alternate_url'], x['name']), axis=1)
     res['published_at'] = res['published_at'].apply(lambda x: str(x)[:10])
     res['description'] = res['description'].apply(lambda x: x[:100]) + '...'
-    df = res[['job','employer','description','published_at']].reset_index(drop=True).head(10).to_html(escape=False)
-    
-    st.write(df, unsafe_allow_html=True)
+    df = (
+        res
+#         [(res['salary_from']>=salary[0]) & (res['area']==city[0])  & (res['experience']==exp)]
+        [['job','employer','description','similarity','published_at']].reset_index(drop=True).rename(
+        columns = {'job':'Вакансия',
+                   'employer':'Работодатель',
+                   'description':'Описание',
+                   'similarity':'Сходство',
+                   'published_at':'Дата публикации',
+                  }
+        )
+    ).sort_values('Сходство', ascending=False).head(10).reset_index(drop=True)
+    df['Сходство'] = df['Сходство'].astype(str) + '%'
+                   
+    st.write(df.to_html(escape=False), unsafe_allow_html=True)
 
     
 # Page 3    
@@ -88,7 +122,7 @@ if page=='Интересная статистика':
     
     # Sidebar
     st.sidebar.subheader('Фильтр параметров')
-    city = st.sidebar.multiselect('Выберите город(а) для визуализации статистики', ['Москва', 'Санкт-Петербург'], default='Москва')
+    city = st.sidebar.multiselect('Город(а) для визуализации статистики', ['Москва', 'Санкт-Петербург'], default='Москва')
     
     # Stats
     with st.spinner('Обработка данных'):
@@ -174,27 +208,46 @@ if page=='Интересная статистика':
 
         st.plotly_chart(fig1, use_container_width=True)
         
-        ## 7
-        multi = alt.selection_multi(fields=['Зарплата','Количество вакансий'], bind='legend')
-        map1=alt.Chart(counties).mark_geoshape(
-            stroke='white'
-        ).encode(
-            color=alt.Color('Зарплата:Q', scale=alt.Scale(scheme="blues")),
-            tooltip=['Зарплата:Q', 'Количество вакансий:Q']
-        ).transform_lookup(
-            lookup='id',
-            from_=alt.LookupData(source, 'id', ['Зарплата', 'Количество вакансий'])
-        ).properties(
-            width=600,
-            height=700
-        ).add_selection(
-            multi
-        ).properties(
-            title=f"Средняя зарплата по округам города {city[0]}"
-        )
+## Page 4
+if page=='Карта':
+    st.title('Карта')
+    
+    # Sidebar
+    st.sidebar.subheader('Фильтр параметров')
+    city = st.sidebar.selectbox('Город', ['Москва'])
+    filt = st.sidebar.selectbox('Параметр', ['Зарплата','Количество вакансий','Вакансии'])
+    title = f"Средняя зарплата по округам города {city}" if filt=='Зарплата' else f"Количество вакансий в округах города {city}"
+    # Stats
+    if filt != 'Вакансии':
+        with st.spinner('Обработка данных'):
+            multi = alt.selection_multi(fields=['Муниципальный округ', filt], bind='legend')
+            map1=alt.Chart(counties).mark_geoshape(
+                stroke='white'
+            ).encode(
+                color=alt.Color(filt+':Q', scale=alt.Scale(scheme="tealblues"), legend=alt.Legend(direction='horizontal',legendX=400, legendY=550, orient='none',gradientLength=200)),
+                tooltip=['Муниципальный округ:O', filt+':Q']
+            ).transform_lookup(
+                lookup='id',
+                from_=alt.LookupData(source, 'id', ['Муниципальный округ', filt])
+            ).properties(
+                width=600,
+                height=700
+            ).add_selection(
+                multi
+            ).properties(
+                title=title
+            ).configure_title(fontSize=14)
 
 
-        st.altair_chart((map1).configure_view(strokeWidth=0), use_container_width=True)
+            st.altair_chart((map1).configure_view(strokeWidth=0), use_container_width=True)
+            
+    if filt == 'Вакансии':
+        fig = px.scatter_mapbox(map_data, lat="lat", lon="lng", hover_name="name",
+                                hover_data=["salary_from", "employment","experience"],
+                                color_discrete_sequence=["#636EFA"], zoom=8, height=500, opacity=0.2,
+                                title='<b>Расположение всех вакансий на карте</b>')
+        fig.update_layout(mapbox_style="light", mapbox_accesstoken=token)
+        fig.update_layout(margin={"r":0,"t":40,"l":0,"b":0}).update_layout(title_font_size=14)
+
         
-        
-
+        st.plotly_chart(fig)
