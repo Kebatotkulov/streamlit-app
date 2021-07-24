4226c57cc62b204791eaa0a9e16af9a9cd55b2a6
Sarvar-Anvarov <53121543+Sarvar-Anvarov@users.noreply.github.com>
2021-07-24 17:23:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sentence_transformers import SentenceTransformer
 from scipy import stats
 import psutil, os, gc, json
 from warnings import filterwarnings
-#from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity
 
 filterwarnings('ignore')
 
@@ -92,25 +92,25 @@ if page=='Поиск вакансий':
     
     results, top_k, vector = search(str(text))
     res = data.loc[top_k[1][0]]
-    #res['similarity'] = [cosine_similarity(vector, [full.loc[top_k[1][0]].loc[i]])[0][0] for i in full.loc[top_k[1][0]].index]
+    res['similarity'] = [cosine_similarity(vector, [full.loc[top_k[1][0]].loc[i]])[0][0] for i in full.loc[top_k[1][0]].index]
     
-    #res['similarity'] = (res['similarity']*100).round(1)
+    res['similarity'] = (res['similarity']*100).round(1)
     res['job'] = res.apply(lambda x: make_clickable(x['alternate_url'], x['name']), axis=1)
     res['published_at'] = res['published_at'].apply(lambda x: str(x)[:10])
     res['description'] = res['description'].apply(lambda x: x[:100]) + '...'
     df = (
         res
 #         [(res['salary_from']>=salary[0]) & (res['area']==city[0])  & (res['experience']==exp)]
-        [['job','employer','description','published_at']].reset_index(drop=True).rename(
+        [['job','employer','description','similarity','published_at']].reset_index(drop=True).rename(
         columns = {'job':'Вакансия',
                    'employer':'Работодатель',
                    'description':'Описание',
-                   #'similarity':'Сходство',
+                   'similarity':'Сходство',
                    'published_at':'Дата публикации',
                   }
         )
-    ).head(10).reset_index(drop=True)#.sort_values('Сходство', ascending=False)
-    #df['Сходство'] = df['Сходство'].astype(str) + '%'
+    ).sort_values('Сходство', ascending=False).head(10).reset_index(drop=True)
+    df['Сходство'] = df['Сходство'].astype(str) + '%'
                    
     st.write(df.to_html(escape=False), unsafe_allow_html=True)
 
