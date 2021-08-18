ce3ad4ce7e13a372feb8379e9afa6fbba20305f8
Sarvar-Anvarov <53121543+Sarvar-Anvarov@users.noreply.github.com>
2021-08-18 15:45:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ if page=='Поиск вакансий':
     
     results, top_k, vector = search(str(text))
     res = data.loc[top_k[1][0]]
-    st.write(vector.shape, np.array(full.loc[0]).shape)
+    st.write(np.reshape(vector, (1,768)).shape, np.reshape(np.array(full.loc[0]), (1,768)).shape)
     res['similarity'] = [cosine_similarity(np.reshape(vector, (1,768)), np.reshape([full.loc[top_k[1][0]].loc[i]], (1,768)))[0][0] for i in full.loc[top_k[1][0]].index]
     
     res['similarity'] = (res['similarity']*100).round(1)
